b063c2f67b7d0a7513fd173a700a30efa474cd9b
Alexander Artemiev <alexanderma1983@gmail.com>
2025-12-31 08:23:03 -0500

    actualizacion del backend y frontend 31/12/2025

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const isEmail = require('validator/lib/isEmail');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   email: {
